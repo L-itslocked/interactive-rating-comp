6db4ec128d6611c69d3279f8fdc771d0ee4f5545
“Laura <114937668+L-itslocked@users.noreply.github.com>
2023-07-26 23:04:33 -0400

    Fix script.js

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-let ratings = document.querySelector(".circle");
+const ratings = document.querySelectorAll("input");
 const ratingCard = document.getElementById("rating-state");
-const btn = document.getElementById("button");
 const thankYou = document.querySelector(".hidden");
-const ratingSelection = document.getElementById("rating-selection");
+const selected = document.getElementById("rating-selection");
 
 function showThankYou() {
   ratingCard.style.display = "none";
   thankYou.classList.remove("hidden");
-
-  ratings.forEach((rating) => {
-    rating.addEventListener("click", () => {
-      ratingSelection.innerHTML = rating.innerHTML;
-    });
-  });
+  ratings.value = selected.innerHTML;
 }
-
-btn.addEventListener("click", showThankYou);
